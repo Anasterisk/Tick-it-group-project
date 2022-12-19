5b5b2df3403f1f1a4041457559cd8d2ec8726cb0
Alvin Toch <Alvinstoch@live.com>
2022-12-18 20:54:39 -0800

    Serializers complete

--- tickit_project/tickit/models.py
+++ tickit_project/tickit/models.py
@@ -6,10 +6,9 @@ class Venue(models.Model):
     name = models.CharField(max_length=100)
     address= models.TextField(max_length=200)
     description= models.TextField(max_length=300, null=True)
-    
-    onsite_parking: models.BooleanField(null=True)
-    capacity: models.IntegerField(null=True, blank=True)
-    photo_url: models.TextField(null=True, max_length=500)
+    onsite_parking= models.BooleanField(null=True)
+    capacity= models.IntegerField(null=True, blank=True)
+    photo_url= models.TextField(null=True, max_length=500)
 
     def __str__(self):
         return self.name
@@ -19,11 +18,10 @@ class Event(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
     description= models.TextField(max_length=500)
-    
-    ASL_interpreter: models.BooleanField(null=True)
-    date: models.CharField(null=True, max_length=100)
-    alcohol_served: models.BooleanField(null=True)
-    photo_url: models.TextField(null=True, max_length=500, blank=True)
+    ASL_interpreter= models.BooleanField(null=True)
+    date= models.CharField(null=True, max_length=100)
+    alcohol_served= models.BooleanField(null=True)
+    photo_url= models.TextField(null=True, max_length=500, blank=True)
 
     def __str__(self):
         return self.name
